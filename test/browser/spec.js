5a187bd2ed9869a225ea145e8972d4d54c3dca95
Mike Bild <mike@mikebild.com>
2013-05-30 12:10:55 +0200

    change to tape

--- test/browser/spec.js
+++ test/browser/spec.js
@@ -1,8 +1,26 @@
-var test = require('tape');
-
+var test = require('tape'),
+	Flow = require('../../lib/cflow');
+	
 test('cflow sequential', function (t) {
-	t.equal("ABC!!!", "ABC!!!");
+	t.equal(data, "ABC!!!");
 	t.end();
+	new Flow(
+		function(){
+			slow("A", this);
+		},
+		function(data){
+			t.equal(data, "A");
+			slow2(data + "B", "!!!", this);
+		},
+		function(data, data2){
+			t.equal(data, "AB");
+			slow(data + "C" + data2, this);
+		},
+		function(data){
+			t.equal(data, "ABC!!!");
+			t.end();
+		}
+	);
 });
 
 function slow(value, callback){
