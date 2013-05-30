67197de2492077c6bf1958071763db600507841d
Mike Bild <mike@mikebild.com>
2013-05-30 11:46:54 +0200

    change to tape

--- test/browser/spec.js
+++ test/browser/spec.js
@@ -1,26 +1,24 @@
-var expect = require("chai").expect,
+var test = require('tape'),
 	Flow = require("../../lib/cflow");
 
-describe("cflow", function () {
-	it("sequential", function (done) {
-		new Flow(
-			function(){
-				slow("A", this);
-			},
-			function(data){
-				expect(data).to.equal("A");
-				slow2(data + "B", "!!!", this);
-			},
-			function(data, data2){
-				expect(data).to.equal("AB");
-				slow(data + "C" + data2, this);
-			},
-			function(data){
-				expect(data).to.equal("ABC!!!");
-				done();
-			}
-		);
-	});
+test('cflow sequential', function (t) {
+	new Flow(
+		function(){
+			slow("A", this);
+		},
+		function(data){
+			t.equal(data, "A");
+			slow2(data + "B", "!!!", this);
+		},
+		function(data, data2){
+			t.equal(data, "AB");
+			slow(data + "C" + data2, this);
+		},
+		function(data){
+			t.equal(data, "ABC!!!");
+			t.end();
+		}
+	);
 });
 
 function slow(value, callback){
